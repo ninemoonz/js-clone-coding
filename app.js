7d2fd945a26e460d9454aba26f9859c12e6404d5
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-06-21 22:00:33 +0900

    js and css

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-// get element by id
-const title = document.querySelectorAll(".grabClass h1");
-console.log(title);
+const h1 = document.querySelector(".grabClass h1");
 
+function handleMouseClick() {
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
+}
 
+h1.addEventListener("click", handleMouseClick);
