c23ab751f5008a29b14b4dd23c7945c9526abfaa
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-06-21 15:50:01 +0900

    selectors

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const age = parseInt(prompt("How old are you?"));
+// get element by id
+const title = document.querySelectorAll(".grabClass h1");
+console.log(title);
 
-if(isNaN(age)){
-    console.log("That's not a number. Enter your age again");
-}
-else if(age <= 18){
-    console.log("You are underage to buy liquors");
-}
-else if(age >= 18 && age <= 50){
-    console.log("You are okay to buy liquors");
-}
-else {
-    console.log("You can drink, but you should be careful");
-}
+
