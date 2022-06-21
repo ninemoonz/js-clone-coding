60be273f006fb76e4fd27e4b12d2a7d1a33e1d96
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-06-21 12:02:03 +0900

    return

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-function sumFunc(a, b) {
-    console.log(a + b);
-}
-
-sumFunc(11, 22);
-sumFunc(12, 14);
-sumFunc(1223, 1214);
-sumFunc(1223, 1214125);
-
-const calc = {
-    sum: function(a, b) {
-        console.log(a + b);
+const myCalculator = {
+    sum: function (a, b) {
+        return (a + b);
     },
-    subt: function(a, b) {
-        console.log(a - b)
+    sub: function (a, b) {
+        return (a - b);
     },
-    mult: function(a, b) {
-        console.log(a * b)
+    mul: function (a, b) {
+        return (a * b);
     },
-    div: function(a, b){
-        console.log(a / b)
+    div: function (a, b) {
+        return (a / b);
     },
-    pow: function(a, b){
-        console.log(a**b)
-    },
+    pow: function (a, b) {
+        return (a**b);
+    }
 };
 
-calc.sum(10, 132);
-calc.subt(30, 21);
-calc.mult(3, 5);
-calc.div(10, 34);
-calc.pow(2, 2);
+const divResult = myCalculator.div(10,2);
+console.log(divResult);
+
+const subResult = myCalculator.sub(20, divResult);
+const mulResult = myCalculator.mul(subResult, 2);
+
+console.log(mulResult);
