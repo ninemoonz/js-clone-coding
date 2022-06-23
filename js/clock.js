35eac68592c73866856c051c71f32f369a625c32
ninemoonz <76804681+ninemoonz@users.noreply.github.com>
2022-06-24 02:18:10 +0900

    interval

---
+++ js/clock.js
@@ -0,0 +1,8 @@
+// interval : every n time
+//setInterval(name of function, millisecond); 
+const clock = document.querySelector("#clock");
+
+function sayHello() {
+    console.log("hello");
+}
+
--- js/greetings.js
+++ js/greetings.js
@@ -8,14 +8,15 @@ const USERNAME_KEY = "username"
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
 }
 
-function paintGreetings(username) {
-    greeting.innerText = `Hello ${username}`;
+function paintGreetings() {
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    
 }
 
 const storedUsername = localStorage.getItem(USERNAME_KEY);
@@ -24,5 +25,5 @@ if(storedUsername === null){
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 }else {
-    paintGreetings(storedUsername);
+    paintGreetings();
 }
